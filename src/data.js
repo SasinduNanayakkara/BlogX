422acb77b616071841e363a0d9fd9a31c4b8e09b
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-01-01 01:46:46 +0530

    add demo data

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const posts = [
+    {
+        id:1,
+        title: "Cloud computing",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Cloud_computing.svg/330px-Cloud_computing.svg.png",
+        desc: "Cloud computing is the on-demand availability of computer system resources, especially data storage and computing power, without direct active management by the user",
+        longDesc: "Cloud computing[1] is the on-demand availability of computer system resources, especially data storage (cloud storage) and computing power, without direct active management by the user.[2] Large clouds often have functions distributed over multiple locations, each location being a data center. Cloud computing relies on sharing of resources to achieve coherence[clarification needed] and economies of scale, typically using a pay-as-you-go model which can help in reducing capital expenses but may also lead to unexpected operating expenses for unaware users."
+    },
+    {
+        id: 2,
+        title: "Artificial intelligence",
+        img: "https://www.fingent.com/wp-content/uploads/fingent-5-1024x555.jpg",
+        desc: "Artificial intelligence (AI) is the ability of a computer or a robot controlled by a computer to do tasks that are usually done by humans because they require human intelligence and discernment.",
+        longDesc: "Artificial intelligence (AI) is intelligence demonstrated by machines, as opposed to natural intelligence displayed by animals including humans. Leading AI textbooks define the field as the study of intelligent agents: any system that perceives its environment and takes actions that maximize its chance of achieving its goals.[a] Some popular accounts use the term artificial intelligence to describe machines that mimic cognitive functions that humans associate with the human mind, such as learning and problem solving, however, this definition is rejected by major AI researchers.",
+    },
+    {
+        id: 3,
+        title: "Java",
+        img: "https://www.filepicker.io/api/file/t12BZqmRoulvCTDhoYie",
+        desc: "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.",
+        longDesc: "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is a general-purpose programming language intended to let programmers write once, run anywhere (WORA),[17] meaning that compiled Java code can run on all platforms that support Java without the need for recompilation.[18] Java applications are typically compiled to bytecode that can run on any Java virtual machine (JVM) regardless of the underlying computer architecture. The syntax of Java is similar to C and C++, but has fewer low-level facilities than either of them. The Java runtime provides dynamic capabilities (such as reflection and runtime code modification) that are typically not available in traditional compiled languages. As of 2019, Java was one of the most popular programming languages in use according to GitHub,[19][20] particularly for client–server web applications, with a reported 9 million developers.",
+    }
+    
+];
